10606c94e29edba93af41699cf8f053ec4e41af8
Rhadbane <rhadbane.idriss@gmail.com>
2024-07-04 21:15:50 +0200

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to make a request to 0.0.0.0:5000/catch_me and get "You got me!" response
-curl -sL -X GET 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
